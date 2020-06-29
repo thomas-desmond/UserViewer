5c3fdbf44f5f39aa5d53a6f9cf27dd3a58e5f233
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2020-06-29 16:44:17 -0700

    Added CORS policy; changed launch URL

--- UserDatabase/Startup.cs
+++ UserDatabase/Startup.cs
@@ -27,6 +27,13 @@ namespace UserDatabase
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("OpenCorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
             services.AddControllers();
 
             services.AddDbContext<UserDatabaseContext>(options =>
@@ -36,6 +43,7 @@ namespace UserDatabase
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("OpenCorsPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
