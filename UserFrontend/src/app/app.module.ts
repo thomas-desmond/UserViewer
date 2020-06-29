ce161942a7896f39e964450dec12600a9cbdc69a
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2020-06-29 16:57:04 -0700

    Created User Display componenet.

--- UserFrontend/src/app/app.module.ts
+++ UserFrontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDisplayComponent } from './user-display/user-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDisplayComponent
   ],
   imports: [
     BrowserModule,
