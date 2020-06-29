cf86091082bb23945cb0f5dc2a9ca4cab770fc1a
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2020-06-29 15:30:23 -0700

    Initial commit with basic user model and generated user controller

---
+++ UserDatabase/Data/UserDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UserDatabase.Models;
+
+namespace UserDatabase.Data
+{
+    public class UserDatabaseContext : DbContext
+    {
+        public UserDatabaseContext (DbContextOptions<UserDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserDatabase.Models.User> User { get; set; }
+    }
+}
---
+++ UserDatabase/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserDatabase.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string Interests { get; set; }
+        public string Picture { get; set; }
+
+
+    }
+}
