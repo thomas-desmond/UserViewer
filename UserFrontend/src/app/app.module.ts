e6d64ddff7ba2ef2cecced95bfed0231da395bad
Thomas Desmond <thomasdes533@gmail.com>
2020-06-29 21:51:46 -0700

    f - WIP beginning work on submit form. minor formatting

--- UserFrontend/src/app/app.module.ts
+++ UserFrontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UserDisplayComponent } from './user-display/user-display.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,9 @@ import { SharedModule } from './shared/shared/shared.module';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- UserFrontend/src/app/user-display/user-display.component.ts
+++ UserFrontend/src/app/user-display/user-display.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-user-display',
@@ -10,12 +11,24 @@ import { User } from '../models/user.model';
 })
 export class UserDisplayComponent implements OnInit {
 
+  searchForm: FormGroup;
   public userList$: Observable<User[]>;
 
-  constructor(private userService: UserService) { 
+  constructor(private userService: UserService, private fb: FormBuilder) { 
+    this.initializeForm();
   }
 
   ngOnInit() {
     this.userList$ = this.userService.getAllUsers();
+  }
+
+  initializeForm(): void {
+    this.searchForm = this.fb.group({
+      searchTerms: new FormControl(''),
+    });
+  }
+
+  handleFormSubmit(): void {
+    console.log("HIT THE SUBMIT");
   }
 }
