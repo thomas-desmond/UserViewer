a0f547e3d41556633e12f99f8dea60df31c7a8a7
Thomas Desmond <thomasdes533@gmail.com>
2020-06-30 20:41:34 -0700

    f - generated add user component from CLI

--- UserFrontend/src/app/app.module.ts
+++ UserFrontend/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserDisplayComponent
+    UserDisplayComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
